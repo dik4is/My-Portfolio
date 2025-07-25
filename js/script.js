de92b4407c0ed4de262d299341ca5770e1a3b28c
dik4is <dik4iskandar@gmail.com>
2025-07-26 00:32:23 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+/*========== TYPING ANIMATION JS ==========*/
+var typed = new Typed(".typing",{
+    strings:["Web Design", "Web Developer", "Gaphic Design", "IT Support", "Network Security", "Programer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+/*========== TYPING ANIMATION JS ==========*/
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll(".li"),
+totalNavList = navList.length;
+for(let i=0; i<totalNavList; i++)
+{
+    const = navList[i].querySelector(".a");
+    a.addEventListener("click", function()
+    {
+        for(let j=0; j<totalNavList; j++)
+        {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+    })
+}
+
+
+
 
